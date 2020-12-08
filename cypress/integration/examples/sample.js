d7488af29624f93f5b5c45a9b828b9f5a2aade95
Badri Baindla <bbaindla@bbaindla-ltm.internal.salesforce.com>
2020-12-08 09:32:43 -0500

    dashboard qa

--- cypress/integration/examples/sample.js
+++ cypress/integration/examples/sample.js
@@ -2,9 +2,8 @@ describe('DemoSuite', function()
 {
     it('landingpage',function()
     {
-    cy.visit('https://go.edmodo.com/teachers/')
-    cy.title().should('eq','Teachers - Edmod')
-    cy.get(".fusion-button-text").type("Teacher Account")
+    cy.visit('https://new.edmodoqabranch.com/')
+    cy.title().should('eq','Edmodo')
     })
 
   })
