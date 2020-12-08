73a0f4571579745490b56998dd6b02fa0afbd990
Badri Baindla <bbaindla@bbaindla-ltm.internal.salesforce.com>
2020-12-08 08:30:40 -0500

    dem commit

---
+++ cypress/integration/examples/sample.js
@@ -0,0 +1,8 @@
+describe('DemoSuite', function()
+{
+    it('landingpage',function()
+    {
+    cy.visit('https://go.edmodo.com/teachers/')
+    cy.title().should('eq','Teachers - Edmodo')
+    })
+  })
