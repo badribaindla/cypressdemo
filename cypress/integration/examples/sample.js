38fff6828f7bdc163933df34191fb5d50890518e
Badri Baindla <bbaindla@bbaindla-ltm.internal.salesforce.com>
2020-12-08 09:26:58 -0500

    dashboard -ve

--- cypress/integration/examples/sample.js
+++ cypress/integration/examples/sample.js
@@ -3,7 +3,7 @@ describe('DemoSuite', function()
     it('landingpage',function()
     {
     cy.visit('https://go.edmodo.com/teachers/')
-    cy.title().should('eq','Teachers - Edmodo')
+    cy.title().should('eq','Teachers - Edmod')
     cy.get(".fusion-button-text").type("Teacher Account")
     })
 
